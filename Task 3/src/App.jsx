e79a6f76443a8131dc29e077b58472d18c84644d
suraniharsh <harshsurani00@gmail.com>
2025-02-11 14:04:55 +0530

    Task 3

---
+++ Task 3/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Footer from './Components/Footer'
+import Header from './Components/Header'
+import MainContent from './Components/MainContent'
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <MainContent/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ Task 3/src/Components/MainContent.jsx
@@ -0,0 +1,13 @@
+const MainContent = () => {
+  return (
+    <main>
+      <h2>Main Content</h2>
+      <p>
+        Welcome to my portfolio! Here, you'll find my projects, skills, and
+        experiences.
+      </p>
+    </main>
+  );
+};
+
+export default MainContent;
