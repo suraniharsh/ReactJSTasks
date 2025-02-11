a253556d26ea205005cc3271102b803cd0a57d1f
suraniharsh <harshsurani00@gmail.com>
2025-02-11 14:25:25 +0530

    Task 7

---
+++ Task 7/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import UserList from './Components/UserList'
+import ProductList from './Components/ProductList'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Welcome to the Lists Page</h1>
+      <UserList />
+      <ProductList />
+    </>
+  );
+}
+
+export default App
---
+++ Task 7/src/Components/UserList.jsx
@@ -0,0 +1,22 @@
+const UserList = () => {
+  const users = [
+    { id: 1, name: "Alice", age: 25 },
+    { id: 2, name: "Bob", age: 30 },
+    { id: 3, name: "Charlie", age: 22 },
+  ];
+
+  return (
+    <div>
+      <h2>User List</h2>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name}, {user.age} years old
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UserList;
